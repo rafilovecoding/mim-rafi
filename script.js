8dbd7f0de85f78383757fb341b62bc2b501d2a8c
rafilovecoding <azmainislamrafi@gmail.com>
2026-07-19 00:18:58 +0600

    Implement moveButton and yesAnswer functions
    Added moveButton function to randomize button position and display elements.

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function goBack(pageNumber) {
     document.getElementById("page" + (pageNumber - 1)).classList.remove("hidden");
 
 }
+function moveButton() {
+
+    const btn = document.getElementById("noBtn");
+
+    btn.style.position = "absolute";
+
+    btn.style.left = Math.random() * 250 + "px";
+
+    btn.style.top = Math.random() * 500 + "px";
+
+    document.getElementById("slapImg").style.display = "block";
+
+    document.getElementById("funText").style.display = "block";
+}
+
+function yesAnswer() {
+
+    alert("ইয়ে! 💜");
+}
